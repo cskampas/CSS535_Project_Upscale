2b95842a062f672248a4a1ce94f1543665b1ff4b
cskam <cskam@OPPENHEIMER>
2023-03-04 13:59:14 -0800

    Add project files.

---
+++ CSS535_Project_Upscale/CSS_535_Project_Upscale.cu
@@ -0,0 +1,14 @@
+﻿#if _MSC_VER
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#endif
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
