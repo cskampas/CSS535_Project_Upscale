51d3c31f68704381d64a9d64e257dd733e64a479
Chris Kampas <cskampas@outlook.com>
2023-03-05 07:50:58 -0800

    work in progress

--- CSS535_Project_Upscale/bitmap.cpp
+++ CSS535_Project_Upscale/bitmap.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+int Bitmap::padSide()
+{
+	return  (4 - (this->width * 3) % 4) % 4;
+}
+
 bool Bitmap::readFromFile(const char* filepath)
 {
 	cout << "read" << endl;
@@ -18,15 +23,12 @@ bool Bitmap::readFromFile(const char* filepath)
 	{
 		cout << "error" << endl;
 	}
-	unsigned char fileHeader[14];
 	unsigned char metadataHeader[40];
-	infile.read(reinterpret_cast<char*>(fileHeader), 14);
+	infile.ignore(14);
 	infile.read(reinterpret_cast<char*>(metadataHeader), 40);
-	int fSize = fileHeader[2] + (fileHeader[3] << 8) + (fileHeader[4] << 16) + (fileHeader[5] << 24);
 	this->width = metadataHeader[4] + (metadataHeader[5] << 8) + (metadataHeader[6] << 16) + (metadataHeader[7] << 24);
 	this->height = metadataHeader[8] + (metadataHeader[9] << 8) + (metadataHeader[10] << 16) + (metadataHeader[11] << 24);
 	this->imageData = new unsigned char[this->width * this->height * 3];
-	int padsize = ((4 - (this->width * 3) % 4) % 4);
 
 	for (int y = 0; y < this->height; ++y)
 	{
@@ -39,33 +41,57 @@ bool Bitmap::readFromFile(const char* filepath)
 			imageData[index] = pixel[0];
         	imageData[index + 1] = pixel[1];
         	imageData[index + 2] = pixel[2];
-
-			// if (pixel[0] == 255 && pixel[1] == 255 && pixel[2] == 255)
-			// 	cout << " ";
-			// else if (pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 0)
-			// 	cout << "X";
-			// else if (pixel[2] == 255)
-			// 	cout << "R";
-			// else if (pixel[1] == 255)
-			// 	cout << "G";
-			// else if (pixel[0] == 255)
-			// 	cout << "B";
-			// else
-			// 	cout << "?";
 		}
-		infile.ignore(padsize);
-		// cout << endl;
+
+		infile.ignore(padSide());
 	}
 
 	infile.close();
-	cout << fileHeader << endl;
 
 	return true;
 }
 
 bool Bitmap::writeToFile(const char* filepath)
 {
+	ofstream outfile;
 	cout << "write" << endl;
+	outfile.open(filepath, ios::out | ios::binary);
+	unsigned char header[14];
+	header[0] = 0x42;
+	header[1] = 0x4d;
+	header[2] = 0x66;
+	header[3] = 0x75;
+	header[4] = 0x00;
+	header[5] = 0x00;
+	header[6] = 0x00;
+	header[7] = 0x00;
+	header[8] = 0x00;
+	header[9] = 0x00;
+	header[10] = 0x36;
+	header[11] = 0x00;
+	header[12] = 0x00;
+	header[13] = 0x00;
+	outfile.write(reinterpret_cast<char*>(header), 14);
+	unsigned char metadata[40];
+	for (int i = 0; i < 40; ++i)
+	{
+		metadata[i] = 0x00;
+	}
+	metadata[0] = 0x28;
+	metadata[4] = this->width;
+	metadata[5] = this->width >> 8;
+	metadata[6] = this->width >> 16;
+	metadata[7] = this->width >> 24;
+	metadata[8] = this->height;
+	metadata[9] = this->height >> 8;
+	metadata[10] = this->height >> 16;
+	metadata[11] = this->height >> 24;
+	metadata[12] = 0x1;
+	metadata[14] = 0x18;
+	outfile.write(reinterpret_cast<char*>(metadata), 40);
+	int size = 3 * sizeof(char) * this->width * this->height + this->width * padSide();
+	outfile.write(reinterpret_cast<char*>(imageData), size);
+	outfile.close();
 	return false;
 }
 
